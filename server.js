507fc4ce31c4f44c00421ef22edec8a8ccaeb8e4
Whizma <isak@maatta.se>
2023-03-26 12:35:06 +0200

    post funkar ish, behöver fixa timestamp

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 import app from './lib/server/db/index.js';
 import http from 'http';
-
-app.use(function(req,res,next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5174");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+import './lib/server/db/init-db.js';
 
 // Create a new HTTP server.
 const server = http.createServer(app);
